eb6e510b5965172f997cdff666a8f754b2fa86d0
KarimMohamed2005 <monkey4gamesmmm@gmail.com>
2019-04-16 23:06:36 +0200

    first commit

---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,14 @@
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyCmiDTUx8UjhXDYH2-nu66ivxXRaZmS2mc"];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
